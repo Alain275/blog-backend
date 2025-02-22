7cc678664a784a1b11405330136d35f0673ab63e
Alain275 <alainshema275@gmail.com>
2025-02-22 21:13:13 +0200

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -57,15 +56,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'backend.urls'
 CORS_ALLOW_ALL_ORIGINS = True
 
-
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:3000",  # Your frontend URL
 ]
 
 TEMPLATES = [
@@ -132,17 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = [os.Path.join(BASE_DIR,'staticfiles')]
-# STATIC_URL = '/media/'
-# MEDIA_URL = os.path.join(BASE_DIR,'media')
-# Static files (CSS, JavaScript, Images)
-# Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'  # This is the URL prefix for serving static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where static files will be collected by 'collectstatic'
-STATICFILES_DIRS = []
-  # Additional directories with static files
-
+STATICFILES_DIRS = []  # Additional directories with static files
 
 # Media files (uploads)
 MEDIA_URL = '/media/'  # URL prefix for serving media files
@@ -155,46 +144,40 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directory where media files (upl
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-'DEFAULT_AUTHENTICATION_CLASSES': (
-'rest_framework_simplejwt.authentication.JWTAuthentication',
-)
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
-
 
 SIMPLE_JWT = {
-'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
-'ROTATE_REFRESH_TOKENS': True,
-'BLACKLIST_AFTER_ROTATION': True,
-'UPDATE_LAST_LOGIN': False,
-'ALGORITHM': 'HS256',
-'VERIFYING_KEY': None,
-'AUDIENCE': None,
-'ISSUER': None,
-'JWK_URL': None,
-'LEEWAY': 0,
-'AUTH_HEADER_TYPES': ('Bearer',),
-'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-'USER_ID_FIELD': 'id',
-'USER_ID_CLAIM': 'user_id',
-'USER_AUTHENTICATION_RULE':
-'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-'TOKEN_TYPE_CLAIM': 'token_type',
-'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
-'JTI_CLAIM': 'jti',
-'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+    'ALGORITHM': 'HS256',
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+    'JTI_CLAIM': 'jti',
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
-
- 
 
 AUTH_USER_MODEL = 'api.User'
 
-
 APPEND_SLASH = True
-
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Your frontend URL
