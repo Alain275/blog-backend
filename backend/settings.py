465c0eb2233dabcfd62f124cfedb1598625936ec
Alain275 <alainshema275@gmail.com>
2025-02-23 10:18:31 +0200

    new

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j!#(uxjw9!w_&b21y+&qj$^zn^-gm^a9nh(oojw&_we-roed-t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    os.getenv("RENDER_EXTERNAL_HOSTNAME", ""),
+    "https://blog-backend-1-a8xw.onrender.com"  # Replace with your actual Render domain
+]
 
 
 # Application definition
